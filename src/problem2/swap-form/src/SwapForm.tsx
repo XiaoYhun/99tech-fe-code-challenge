d419b1ae5fdcc99892798f07f3ec7b9abe821f4c
dieppham-ipworld <xiaoyhun@gmail.com>
2026-06-04 21:20:39 +0700

    redeploy

--- src/problem2/swap-form/src/SwapForm.tsx
+++ src/problem2/swap-form/src/SwapForm.tsx
@@ -21,6 +21,7 @@ export default function SwapForm() {
   const { balances } = useTokenBalances();
   const tokenInBalance = useMemo(() => (tokenIn?.symbol ? balances[tokenIn?.symbol] || 0 : 0), [balances, tokenIn]);
   const debouncedAmountIn = useDebounce(amountIn, 500)[0];
+
   const isLoading = swapState === "loading";
 
   const handleSwapClick = () => {
